c0c2b37a0ef903dedcbc264de26f0a52c39216dd
robbytrujillo <robbyikusuma88@gmail.com>
2026-01-10 18:10:18 +0700

    Introduction Humans | Seeder

---
+++ database/seeders/HumanResourcesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
+
+class HumanResourcesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+
+    }
+}
