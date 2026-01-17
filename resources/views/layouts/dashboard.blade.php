42ab764eed6af39dfe32b787019bfd0846f0b069
robbytrujillo <robbyikusuma88@gmail.com>
2026-01-17 16:31:12 +0700

    Introduction Humans | task index feature

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -162,6 +162,7 @@
     <script src="{{ asset('mazer/dist/assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{ asset('mazer/dist/assets/static/js/pages/dashboard.js') }}"></script>
     
+    {{--  Dibutuhkan untuk handle datatable  --}}
     <script src="{{ asset('mazer/dist/assets/extensions/simple-datatables/umd/simple-datatables.js') }}"></script>
     <script src="{{ asset('mazer/dist/assets/static/js/pages/simple-datatables.js') }}"></script>
 </body>
