fc9363cdd069bb4442be8e1d22b408a7b5a05c5b
robbytrujillo <robbyikusuma88@gmail.com>
2026-01-17 20:57:24 +0700

    Introduction Humans | task index feature

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
     //
     public function index() {
-        return view('tasks.index');
+        $tasks = Task::all();
+        dd($tasks);
+
+        return view('tasks.index', compact('tasks'));
     }
 }
