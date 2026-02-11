5cf1b13e977024bfb3bdd9ee60d8326afb214739
robbytrujillo <robbyikusuma88@gmail.com>
2026-02-12 05:39:00 +0700

    Introduction Humans | Beautifying Insights - Total Data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Employee;
+use App\Models\Department;
+use App\Models\Payroll;
+use App\Models\Presence;
+use App\Models\Task;
 
 class DashboardController extends Controller
 {
