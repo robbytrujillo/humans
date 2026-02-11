723497b3d56bc5f82cafae5110f1d47b9864bf0f
robbytrujillo <robbyikusuma88@gmail.com>
2026-02-11 15:25:26 +0700

    Introduction Humans | Handle Menu Active

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -77,56 +77,56 @@
 
                     @if (session('role') === 'HR')
                     <li
-                        class="sidebar-item active " >
+                        class="sidebar-item {{ request()->is('dashboard') ? 'active' : '' }} " >
                         <a href="{{ url('/dashboard') }}" class='sidebar-link'>
                             <i class="bi bi-grid-fill"></i>
                             <span>Dashboard</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('tasks') ? 'active' : '' }}">
                         <a href="{{ url('/tasks') }}" class='sidebar-link'>
                             <i class="bi bi-check-circle-fill"></i>
                             <span>Tasks</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('employees') ? 'active' : '' }}">
                         <a href="{{ url('/employees') }}" class='sidebar-link'>
                             <i class="bi bi-people-fill"></i>
                             <span>Employees</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('departments') ? 'active' : '' }}">
                         <a href="{{ url('/departments') }}" class='sidebar-link'>
                             <i class="bi bi-briefcase"></i>
                             <span>Departments</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('roles') ? 'active' : '' }}">
                         <a href="{{ url('/roles') }}" class='sidebar-link'>
                             <i class="bi bi-tag"></i>
                             <span>Roles</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('presences') ? 'active' : '' }}">
                         <a href="{{ url('/presences') }}" class='sidebar-link'>
                             <i class="bi bi-table"></i>
                             <span>Presences</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('payrolls') ? 'active' : '' }}">
                         <a href="{{ url('/payrolls') }}" class='sidebar-link'>
                             <i class="bi bi-currency-dollar"></i>
                             <span>Payrolls</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('leave-requests') ? 'active' : '' }}">
                         <a href="{{ url('/leave-requests') }}" class='sidebar-link'>
                             <i class="bi bi-shift-fill"></i>
                             <span>Leave Requests</span>
@@ -136,35 +136,35 @@
 
                     @if (in_array(session('role'), ['IT Support','Education','Finance']))
                      <li
-                        class="sidebar-item active " >
+                        class="sidebar-item {{ request()->is('dashboard') ? 'active' : '' }} " >
                         <a href="{{ url('/dashboard') }}" class='sidebar-link'>
                             <i class="bi bi-grid-fill"></i>
                             <span>Dashboard</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('tasks') ? 'active' : '' }}">
                         <a href="{{ url('/tasks') }}" class='sidebar-link'>
                             <i class="bi bi-check-circle-fill"></i>
                             <span>Tasks</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('presences') ? 'active' : '' }}">
                         <a href="{{ url('/presences') }}" class='sidebar-link'>
                             <i class="bi bi-table"></i>
                             <span>Presences</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('payrolls') ? 'active' : '' }}">
                         <a href="{{ url('/payrolls') }}" class='sidebar-link'>
                             <i class="bi bi-currency-dollar"></i>
                             <span>Payrolls</span>
                         </a>
                     </li>
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('leave-requests') ? 'active' : '' }}">
                         <a href="{{ url('/leave-requests') }}" class='sidebar-link'>
                             <i class="bi bi-shift-fill"></i>
                             <span>Leave Requests</span>
@@ -173,7 +173,7 @@
                     @endif
 
                     <li
-                        class="sidebar-item">
+                        class="sidebar-item {{ request()->is('logout') ? 'active' : '' }}">
                         <a href="{{ url('/logout') }}" class='sidebar-link'>
                             <i class="bi bi-box-arrow-right"></i>
                             <span>Logout</span>
